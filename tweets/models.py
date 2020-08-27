f6e1ec62e719c8ddc501633f7cdc953b08921238
kittipanP <kittipan.prasertsang@gmail.com>
2020-08-27 16:30:02 +0700

    back to be0e587

--- tweets/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Tweet(models.Model):
-    # SQL
-    # id = models.AutoField(primaryField_key=True)
-    content = models.TextField(blank=True, null=True)
-    image = models.FileField(upload_to='image/', blank=True, null=True)
