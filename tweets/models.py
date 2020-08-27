884643f016f17d32666187f449d6bfe1afb1e733
kittipanP <kittipan.prasertsang@gmail.com>
2020-08-27 11:37:27 +0700

    Store Data from Django Model

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Tweet(models.Model):
+    # SQL
     # id = models.AutoField(primaryField_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='image/', blank=True, null=True)
